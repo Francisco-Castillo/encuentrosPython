6176973c8628f18a46d400ff909bdd2236ef8c5a
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2018-06-01 22:23:42 -0300

    Hello World

--- hola.py
+++ hola.py
@@ -1,2 +1,8 @@
-#   Funcion
+#   ===========================================================================
+#   Archivo:        hola.py
+#   Author:         Francisco Castillo
+#   Fecha:          01/06/2018
+#   ===========================================================================
+
+#   Funcion que imprime en pantalla el mensaje Hello World
 print("Hello World")
