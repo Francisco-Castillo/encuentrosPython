cef0a691c3fb8ec1a9d22546d97981cff061146f
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2018-06-01 22:16:47 -0300

    Obtener numero y mostrar su anterior y su posterior

---
+++ EncuentroI/ejercicio1.py
@@ -0,0 +1,16 @@
+#   ===========================================================================
+#   *************************** Enunciado 1 ***********************************
+#   Leer un número, determinar y mostrar, el número ingresado, el anterior y el
+#   siguiente
+#   ===========================================================================
+
+def pedir_numero():
+    numero = int(input("Ingrese un numero: "))
+    return numero
+
+numero_actual = pedir_numero()
+numero_anterior=numero_actual-1
+numero_posterior=numero_actual+1
+#   Imprimimos los resultados
+print("\n=======================SECUENCIA RESULTANTE===========================")
+print("\n",numero_anterior,numero_actual,numero_posterior, sep="\n",end="\n\n")
